2550efb0ea1178db12b5d5b0d4555c7b843674b9
Levi Buzolic <levibuzolic@gmail.com>
2020-08-01 16:06:19 +1000

    Fix fzf

--- fzf/config.zsh
+++ fzf/config.zsh
@@ -1,11 +1,5 @@
 export FZF_DEFAULT_OPTS="--ansi --no-mouse --height=20" # Enable colour; disable mouse to allow text selection; run inline instead of fullscreen.
 
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/usr/local/opt/fzf/bin"
-fi
-
 # Auto-completion
 # ---------------
 [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
