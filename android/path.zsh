b7c4a497d81115bc6a25fd6646540d25523a57d4
Levi Buzolic <levibuzolic@gmail.com>
2020-08-25 11:45:19 +1000

    Paths

---
+++ android/path.zsh
@@ -0,0 +1,17 @@
+# export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+export JAVA_HOME="/Applications/Android Studio.app/Contents/jre/jdk/Contents/Home"
+# export ANT_HOME=/usr/local/opt/ant
+# export MAVEN_HOME=/usr/local/opt/maven
+# export GRADLE_HOME=/usr/local/opt/gradle
+export ANDROID_HOME=/Users/$USER/Library/Android/sdk
+# export ANDROID_NDK_HOME=/usr/local/share/android-ndk
+# export INTEL_HAXM_HOME=/usr/local/Caskroom/intel-haxm
+# export ANDROID_SDK_ROOT=$ANDROID_HOME
+
+# export PATH=$JAVA_HOME/bin:$PATH
+# export PATH=$ANT_HOME/bin:$PATH
+# export PATH=$MAVEN_HOME/bin:$PATH
+# export PATH=$GRADLE_HOME/bin:$PATH
+export PATH=$ANDROID_HOME/tools:$PATH
+export PATH=$ANDROID_HOME/platform-tools:$PATH
+# export PATH=$ANDROID_HOME/build-tools/23.0.1:$PATH
