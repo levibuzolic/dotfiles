2e1d81002f4e7fd7b2531b93bcc0dfadbe786057
Levi Buzolic <levibuzolic@gmail.com>
2021-04-27 12:01:07 +1000

    Use ANDROID_SDK_ROOT instead of ANDROID_HOME

--- android/path.zsh
+++ android/path.zsh
@@ -1,4 +1,4 @@
 export JAVA_HOME="/Applications/Android Studio.app/Contents/jre/jdk/Contents/Home"
-export ANDROID_HOME=$HOME/Library/Android/sdk
-export PATH=$ANDROID_HOME/tools:$PATH
-export PATH=$ANDROID_HOME/platform-tools:$PATH
+export ANDROID_SDK_ROOT=$HOME/Library/Android/sdk
+export PATH=$ANDROID_SDK_ROOT/tools:$PATH
+export PATH=$ANDROID_SDK_ROOT/platform-tools:$PATH
